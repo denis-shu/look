2e25ea40c182640ddb8ae4dbc72f367e0da6f974
denis-shu <shumovetsky@gmail.com>
2017-01-29 22:03:07 +0200

    Update AccountController.cs

--- AccountController.cs
+++ AccountController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using MyFirstProject.Models;
+using Project.Models;
 
 namespace MyFirstProject.Controllers
 {
